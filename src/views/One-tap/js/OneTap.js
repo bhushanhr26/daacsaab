980f5fd4ebc1ca2c6c39c10211c1da2859c7bca6
Gantavya Saraswat <gantavya99@gmail.com>
2022-12-15 23:53:02 +0530

    Added symptoms for one-Tap page

--- src/views/One-tap/js/OneTap.js
+++ src/views/One-tap/js/OneTap.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "../css/oneTap.css";
 const oneTap = () => {
+const symptoms=["Headache","Fever","Dry Cough","Wet Cough","Loose Motion","Constipation","Runny Nose","Red Eye","Feeling cold"
+,"Rashes","Itching","Breathlessness","Pain in Knee","Leg Sprain","Cut on Skin","Vomitting","Swollen Eye","Migraine","Unable to Sleep",
+"Loss of appetite","Pain in Kness"];
   return (
     <div>
       <div className="container">
@@ -13,6 +16,8 @@ const oneTap = () => {
       <button className="btn btn-primary">Yes</button>
       <button className="btn btn-secondary">No, I want to see a specialist</button>
       </div>
+      <hr></hr>
+
       </div>
     </div>
   )
